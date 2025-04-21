7eac90c7cab25c6314f2b1afd727833cb69c4c1d
Reza Maulana <rezaosman92@gmail.com>
2025-04-22 06:24:16 +0700

    changed to tokyonight with dark bg

--- init.lua
+++ init.lua
@@ -879,14 +879,15 @@ require('lazy').setup({
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
         styles = {
-          comments = { italic = false }, -- Disable italics in comments
+          --comments = { italic = false }, -- Disable italics in comments
         },
       }
 
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'tokyonight-storm'
+      vim.o.background = 'dark'
     end,
   },
 
